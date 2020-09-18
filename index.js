8ec32138d583ff591119f5c1cd37fc1c5c65280b
Mario Barrios <mariobarriossalazar@outlook.com>
2020-09-18 12:40:55 -0600

    Add create inventory function and associated methods

--- index.js
+++ index.js
@@ -32,8 +32,101 @@ const changeBookStatus = (book, button) => {
   storeLibrary();
 };
 
-const removeBook = (bookIndex) {
+const removeBook = (bookIndex) => {
   localStorage.clear();
   myLibrary.splice(bookIndex, 1);
   storeLibrary();
 };
+
+const addChangeListeners = () => {
+  const changeButtons = document.querySelectorAll('.change-btn');
+  changeButtons.forEach((button) => {
+    button.onclick = () => {
+      const { dataset: { bookIndex } } = button;
+      const book = myLibrary[bookIndex];
+      changeBookStatus(book, button);
+    };
+  });
+};
+
+const addDeleteListeners = () => {
+  const deleteButtons = document.querySelectorAll('.delete-btn');
+  deleteButtons.forEach((button) => {
+    button.onclick = () => {
+      const { dataset: { bookIndex } } = button;
+      const targetBook = document.querySelector(`li[data-book-index="${bookIndex}"]`);
+      removeBook(bookIndex);
+      inventory.removeChild(targetBook);
+    };
+  });
+};
+
+const createInventory = (library) => {
+  inventory.innerHTML = '';
+  library.forEach((book) => {
+    const li = document.createElement('li');
+    li.className = 'column is-full-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd';
+    const bookIndex = library.indexOf(book);
+    li.setAttribute('data-book-index', `${bookIndex}`);
+    const statusColor = book.status === 'Already read!' ? 'is-success' : '';
+    const bookContent = `<div class="card">
+                          <div class="card-image">
+                            <figure class="image is-4by3">
+                              <img src="./assets/images/default-cover.jpg" alt="Book cover">
+                            </figure>
+                          </div>
+                          <div class="card-content">
+                            <div class="media">
+                              <div class="media-left">
+                                <figure class="image is-48x48">
+                                  <img src="./assets/images/default-thumbnail.jpg" alt="Book thumbnail">
+                                </figure>
+                              </div>
+                              <div class="media-content">
+                                <p class="title is-4">${book.title}</p>
+                                <p class="subtitle is-6">${book.author}</p>
+                              </div>
+                            </div>
+                            <div class="content">
+                              Number of pages:
+                              <br>
+                              ${book.pages}
+                            </div>
+                          </div>
+                          <footer class="card-footer">
+                            <button data-book-index="${bookIndex}" class="button ${statusColor} change-btn card-footer-item">${book.status}</button>
+                            <button data-book-index="${bookIndex}" class="button is-danger delete-btn card-footer-item" >Delete</button>
+                          </footer>
+                        </div>`;
+    li.insertAdjacentHTML('beforeend', bookContent);
+    inventory.appendChild(li);
+  });
+  addChangeListeners();
+  addDeleteListeners();
+};
+
+const addBookToLibrary = () => {
+  const title = document.querySelector('[name="book-title"]').value;
+  const author = document.querySelector('[name="book-author"]').value;
+  const pages = document.querySelector('[name="book-pages"]').value;
+  const status = document.querySelector('[name="book-status"]').checked;
+  const book = new Book(title, author, pages, status);
+  myLibrary.push(book);
+  createInventory(myLibrary);
+  resetForm();
+  storeLibrary();
+};
+
+const addNewButton = document.querySelector('#new-book-btn');
+const bookForm = document.querySelector('#book-form');
+
+const bookFormToggle = () => bookForm.classList.toggle('is-hidden');
+addNewButton.addEventListener('click', () => bookFormToggle());
+
+const saveButton = document.querySelector('#save-button');
+saveButton.addEventListener('click', () => {
+  bookFormToggle();
+  addBookToLibrary();
+});
+
+createInventory(myLibrary);
